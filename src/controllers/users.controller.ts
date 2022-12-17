216bcd4ada83e78c58474aed03dcd64dfa1832ce
BaderBC <kontakt@bstrama.com>
2022-12-17 08:05:09 +0100

    nothing special

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,12 +1,13 @@
 import {Request} from "express";
 
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 import {IDatabase} from "pg-promise";
 
 const sha512 = require('js-sha512');
 const twofactor = require("node-2fa");
 
-import { FMail } from '../lib/fmail';
+import {FMail} from '../lib/fmail';
+
 const fmail = new FMail("http://vps.filipton.space:8656", "admin", "AdminPassw0rd1277410641");
 
 module.exports = async function (packages: any) {
@@ -14,7 +15,7 @@ module.exports = async function (packages: any) {
         SECRET
     } = process.env;
 
-    const { express, db, jwt } = packages;
+    const {express, db, jwt} = packages;
     const Router = express.Router();
 
     Router.get('/', function (req: any, res: any) {
@@ -22,10 +23,10 @@ module.exports = async function (packages: any) {
     });
 
     Router.get('/:username', async (req: any, res: any) => {
-        const { username } = req.params;
+        const {username} = req.params;
         console.log(username);
         await db.one("SELECT id, firstname, lastname, username, created_at FROM Users WHERE username = $1", [username])
-            .then(async (user: any) =>{
+            .then(async (user: any) => {
                 console.log(user);
                 let lists = await db.any("SELECT * FROM listings as l WHERE l.id = $1", [user.id]);
 
@@ -36,7 +37,7 @@ module.exports = async function (packages: any) {
                     data: lists
                 });
             })
-            .catch(() =>{
+            .catch(() => {
                 res.status(404).send("User not found");
             });
     });
@@ -44,13 +45,13 @@ module.exports = async function (packages: any) {
     Router.post('/signup', async function (req: any, res: any) {
         let verificationCode: any = uuidv4();
 
-        const { username, email, password, firstname, lastname, localization, tfa } = req.body;
+        const {username, email, password, firstname, lastname, localization, tfa} = req.body;
         const secondsSinceEpoch = Math.round(Date.now() / 1000);
 
 
         let secret: any = {};
-        if(tfa) {
-            const newSecret = twofactor.generateSecret({ name: "Auction House", account: username });
+        if (tfa) {
+            const newSecret = twofactor.generateSecret({name: "Auction House", account: username});
             secret = newSecret;
         }
 
@@ -77,39 +78,39 @@ module.exports = async function (packages: any) {
     });
 
     Router.get('/verify/:code', async function (req: any, res: any) {
-        const { code } = req.params;
+        const {code} = req.params;
 
         await db.one("UPDATE Users SET verified = true WHERE verified = false AND verification = $1 RETURNING id", [code])
             .then(() => {
                 return res.status(200).send("VERIFIED.... RETURN TO MAIN PAGE");
             })
-            .catch(() =>{
-               return res.status(500).send("ERROR");
+            .catch(() => {
+                return res.status(500).send("ERROR");
             });
     });
 
     Router.post('/signin', async function (req: any, res: any) {
-        const { email, password, tfa } = req.body;
+        const {email, password, tfa} = req.body;
 
-        if(!email.includes('@') || !email.includes('.') || !(email.length >= 5)) return res.status(400).send('bad email');
+        if (!email.includes('@') || !email.includes('.') || !(email.length >= 5)) return res.status(400).send('bad email');
         //if(password.length === 0) return res.status(400).send('password is too short');
 
         await db.one("SELECT id, email, firstname, lastname, localization, tfa, username, created_at FROM Users WHERE email = $1 AND password = $2",
             [email, sha512(password)])
-            .then((user: any) =>{
-                if(user.tfa) {
-                    if(!tfa) return res.status(400).send("SEND 2FA");
+            .then((user: any) => {
+                if (user.tfa) {
+                    if (!tfa) return res.status(400).send("SEND 2FA");
 
-                    const { delta } = twofactor.verifyToken(user.tfa, tfa);
-                    if(delta < 0) return res.status(401).send("BAD 2FA");
+                    const {delta} = twofactor.verifyToken(user.tfa, tfa);
+                    if (delta < 0) return res.status(401).send("BAD 2FA");
                 }
 
                 return res.status(200).json({
                     token: jwt.sign({data: user}, SECRET)
                 });
             })
-            .catch(() =>{
-               return res.status(500).send("ERROR");
+            .catch(() => {
+                return res.status(500).send("ERROR");
             });
     });
 
